1feada097e7399a0fff1700e03f71bdefbd1dc82
ddd <ss@ee.com>
2016-09-28 23:04:21 +0800

    dddd

--- Test/ConsoleApplication1/ConsoleApplication1/Program.cs
+++ Test/ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
 
-            string str = "d";
+            string str = "166666666666666666666d";
             str = "dddd";
             str = "1";
 
